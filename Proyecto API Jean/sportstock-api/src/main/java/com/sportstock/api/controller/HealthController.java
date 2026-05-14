ff172a1d22722b2f41f6de9b64c726f66eba3318
JeanOrtiz98 <jean_castanedaor@fet.edu.co>
2026-05-14 10:29:00 -0500

    Change health endpoint to '/health'
    Error en el codigo

--- Proyecto API Jean/sportstock-api/src/main/java/com/sportstock/api/controller/HealthController.java
+++ Proyecto API Jean/sportstock-api/src/main/java/com/sportstock/api/controller/HealthController.java
@@ -1,23 +1,26 @@
+package com.sportstock.api.controller;
+
 // ==============================
 // IMPORTACIONES
 // ==============================
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 // ==============================
 // CONTROLADOR PRINCIPAL
 // ==============================
 
 @RestController
+@RequestMapping("/api")
 public class HealthController {
 
     // ==============================
     // ENDPOINT DE PRUEBA
     // ==============================
 
-    @GetMapping("/")
+    @GetMapping("/health")
     public String health() {
         return "SportStock API funcionando correctamente 🚀";
     }

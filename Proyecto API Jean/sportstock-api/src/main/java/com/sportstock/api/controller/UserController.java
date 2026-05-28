11349dcdf6d8a5e8004f2a3cbb3e77d70a0f6189
Jean Carlos <jean_castanedaor@fet.edu.co>
2026-05-28 18:13:31 -0500

    Etiqueta de usuarios

--- Proyecto API Jean/sportstock-api/src/main/java/com/sportstock/api/controller/UserController.java
+++ Proyecto API Jean/sportstock-api/src/main/java/com/sportstock/api/controller/UserController.java
@@ -6,26 +6,51 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// ==============================
+// CONTROLADOR DE USUARIOS
+// Maneja las peticiones HTTP
+// ==============================
+
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin("*")
 public class UserController {
 
+    // ==============================
+    // SERVICIO DE USUARIO
+    // ==============================
+
     private final UserService userService;
+
+    // ==============================
+    // INYECCIÓN DE DEPENDENCIAS
+    // ==============================
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    // ==============================
+    // OBTENER TODOS LOS USUARIOS
+    // ==============================
 
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
     }
 
+    // ==============================
+    // CREAR USUARIO (ADMIN)
+    // ==============================
+
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
+
+    // =========================================================
+    // CREAR USUARIO DESDE EL LOGIN/ AUTOREGISTRO DE EMPLEADOS
+    // =========================================================
 
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
@@ -35,6 +60,10 @@ public class UserController {
         return userService.createUser(user);
     }
 
+    // ==============================
+    // ACTUALIZAR PRODUCTO
+    // ==============================
+
     @PutMapping("/{id}")
     public User updateUser(
             @PathVariable Long id,
@@ -42,6 +71,10 @@ public class UserController {
     ) {
         return userService.updateUser(id, user);
     }
+
+    // ==============================
+    // ELIMINAR USUARIO
+    // ==============================
 
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
